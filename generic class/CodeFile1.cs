ff3f3266407769bf18537ee1566d2d877ad39fc4
CORP\Neeharika.Pathuri <neehu395@gmail.com>
2017-02-02 07:46:07 +0530

    update

--- generic class/CodeFile1.cs
+++ generic class/CodeFile1.cs
@@ -7,79 +7,79 @@ using System.Threading.Tasks;
 
 class myCollection<Array>
 {
-public Array[] items;
-public int Length;
-public myCollection()
-{
-
-}
-public myCollection(int Length)
-{
-    this.Length = Length;
-    this.items = new Array[Length];
-}
-public Array this[int index]
-{
-
-    get
+    public Array[] collection;
+    public int Length;
+    public myCollection()
     {
-        var element = items[index];
 
-        return element;
     }
-    set
+    public myCollection(int Length)
     {
-        if ((index >= 0) && (index < 9))
-
-
-            items[index + 1] = value;
-
-        else
-            if ((index <= 0) && ((index < 9) && (index >= -10)))
-
-
-            items[10 - (System.Math.Abs(index))] = value;
+        this.Length = Length;
+        this.collection = new Array[Length];
     }
+    public Array this[int index]
+    {
 
-}
+        get
+        {
+            var element = collection[index];
+
+            return element;
+        }
+        set
+        {
+            if ((index >= 0) && (index < 9))
+
+
+                collection[index + 1] = value;
+
+            else
+                if ((index <= 0) && ((index < 9) && (index >= -10)))
+
+
+                collection[10 - (System.Math.Abs(index))] = value;
+        }
+
+    }
 }
 
 namespace generic_class
 {
-class Execution
-{
-
-    static void Main(string[] args)
+    class Execution
     {
-        myCollection<int> positiveIndexarray = new myCollection<int>(10);
-        myCollection<int> negativeIndexarray = new myCollection<int>(10);
-        for (int positiveIndex = 0; positiveIndex <= 9; positiveIndex++)
+
+        static void Main(string[] args)
         {
-            positiveIndexarray[positiveIndex] = positiveIndex + 1;
+            myCollection<int> positiveIndexarray = new myCollection<int>(10);
+            myCollection<int> negativeIndexarray = new myCollection<int>(10);
+            for (int positiveIndex = 0; positiveIndex <= 9; positiveIndex++)
+            {
+                positiveIndexarray[positiveIndex] = positiveIndex + 1;
+            }
+
+            Console.WriteLine("The output when index is positive");
+
+            for (int positiveIndex = 0; positiveIndex < 10; positiveIndex++)
+            {
+                Console.WriteLine(positiveIndexarray[positiveIndex]);
+            }
+
+            Console.WriteLine("The output when index is Negative");
+
+            for (int negativeIndex = 0; negativeIndex >= -9; negativeIndex--)
+            {
+                negativeIndexarray[negativeIndex] = positiveIndexarray[System.Math.Abs(negativeIndex)];
+
+            }
+            for (int negativeIndex = 0; negativeIndex <= 9; negativeIndex++)
+            {
+                Console.WriteLine(negativeIndexarray[negativeIndex]);
+            }
+
+            Console.ReadKey();
         }
-
-        Console.WriteLine("The output when index is positive");
-
-        for (int positiveIndex = 0; positiveIndex < 10; positiveIndex++)
-        {
-            Console.WriteLine(positiveIndexarray[positiveIndex]);
-        }
-
-        Console.WriteLine("The output when index is Negative");
-
-        for (int negativeIndex = 0; negativeIndex >= -9; negativeIndex--)
-        {
-            negativeIndexarray[negativeIndex] = positiveIndexarray[System.Math.Abs(negativeIndex)];
-
-        }
-        for (int negativeIndex = 0; negativeIndex <= 9; negativeIndex++)
-        {
-            Console.WriteLine(negativeIndexarray[negativeIndex]);
-        }
-
-        Console.ReadKey();
     }
-}
 }
 
 
